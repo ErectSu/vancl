4bc384a79891244d4e3af310a2e4857d7f99b90c
ErectSu <2469070295@qq.com>
2018-10-13 19:56:16 +0800

    modify2

--- js/productdetali.js
+++ js/productdetali.js
@@ -1,4 +1,246 @@
-localStorage.removeItem('minelist');
+// console.log(document.cookie);
+// var _cookie=new OprationCookie();
+// var hobbyid=_cookie.getCookie('hobbyid');
+// console.log(hobbyid);//001
+// var options = {
+//   method: "get",
+//   async: true,
+//   data: {
+//       'sid':hobbyid
+//   },
+//   success: function(data){
+      
+//       data=JSON.parse(data);
+//       console.log(data);//数组对象；
+//   //     [
+//   //         {
+//   //            "sid":"001",
+//   //            "introduce":"ERYAN尔晏 抽象撞色纹中筒袜(男款)2双装",
+//   //            "price":"78.00",
+//   //             "src":"images/shop/load01.jpg"
+//   //         },
+//   //         {
+//   //            "sid":"002",
+//   //            "introduce":"ERY 克尔晏 画风撞色纹中筒袜(男款)1双装",
+//   //            "price":"88.00",
+//   //             "src":"images/shop/load02.jpg"
+//   //         }
+//   //    ]
+//       var str=[];
+//       for(var i=0;i<data.length;i++){
+//           console.log(data[i].sid);
+//           if(data[i].sid == $id){
+//               str += `
+              
+//               <div class="danpin_colLef newclear">
+//               <div class="danpinLeft">
+//                   <div class="smallImg">
+//                       <div class="smallImgUp upper" style='visibility: hidden;'></div>
+//                       <ul id='imageMenu'>
+//                           <li name='item-item-img-1' class="track" id='onclickImg'>
+//                               <span class="SpriteSmallImgs" name='' style='background: url(${data[i].srcSmall01}) no-repeat'>
+//                                   &nbsp;
+//                               </span>
+//                           </li>
+//                           <li name='item-item-img-2' class="track">
+//                               <span class="SpriteSmallImgs" name='' style='background: url(${data[i].srcSmall02}) no-repeat'>
+//                                   &nbsp;
+//                               </span>
+//                           </li>
+//                           <li name='item-item-img-2' class="track">
+//                               <span class="SpriteSmallImgs" name='' style='background: url(images/small/small01_03.jpg) no-repeat'>
+//                                   &nbsp;
+//                               </span>
+//                           </li>
+//                           <li name='item-item-img-2' class="track">
+//                               <span class="SpriteSmallImgs" name='' style='background: url(images/small/small01_04.jpg) no-repeat'>
+//                                   &nbsp;
+//                               </span>
+//                           </li>
+//                           <li name='item-item-img-2' class="track">
+//                               <span class="SpriteSmallImgs" name='' style='background: url(images/small/small01_05.jpg) no-repeat'>
+//                                   &nbsp;
+//                               </span>
+//                           </li>
+//                       </ul>
+//                       <div class="smallImgDown downer" style='visibility: hidden;'></div>
+//                   </div>
+//               </div>
+//               <div class="danpinColCenter">
+//                   <div class="bigImg">
+//                       <img id="midImg" src="images/big/big01_01.jpg" title="" alt="">
+//                       <span class="filter"></span>
+//                   </div>
+//               </div>
+//               <div class="showlargerImg">
+//                   <div class="largerImg">
+//                       <img id="largerImg" src="images/larger/larger01_01.jpg" title="" alt="">
+//                   </div>
+//               </div>
+//           </div>
+//           <div id="danpinRight" class="danpinRight newclear" style="top: 0px; display: block;">
+
+
+//               <div class="danpinFixedRightContent">
+//                   <div name="Normal" id="pricearea">
+//                       <span class="blank10"></span>
+//                       <div class="cuxiaoPrice ">
+//                           <span class="tehuiMoney" style="line-height: 26px;">
+//                               <span>售价：</span>
+//                               <span style="font-family: '微软雅黑';">￥
+//                                   <strong>78.00</strong>
+//                               </span>
+//                           </span>
+//                           <a id="czwenan" href="http://cz.vancl.com/DepositPre.aspx" target="_blank" style="float: left;
+//                                   height: 26px; display: inline-block; margin-left: 20px; line-height: 26px; margin-top: 7px;
+//                                   color: #a10000;">充100返100，点击充值
+//                           </a>
+//                       </div>
+//                       <span class="blank10"></span>
+//                   </div>
+//                   <div class="selectArea">
+//                       <div class="selColorArea">
+//                           <span class="blank10"></span>
+//                           <div class="danpinColor_Select">
+//                               <div class="danpinColorTitle" style="line-height: 18px;">
+//                                   <p>
+//                                       颜色：</p>
+//                               </div>
+//                               <div class="selColor">
+//                                   <ul>
+//                                       <li id="onlickColor" name="6382975" title="藏青色+暖褐" class="">
+//                                           <div class="colorBlock" name="True">
+//                                               <a class="track" name="item-item-select-color_1" href="6382975.html?ref=item_color_6382975">
+//                                                   <span class="SpriteColors" style="background-position: 0px -0px">&nbsp;</span>
+//                                                   <p>
+//                                                       藏青色+暖褐</p>
+//                                               </a>
+//                                           </div>
+//                                           <span class="colorHoverok"></span>
+
+//                                       </li>
+
+//                                   </ul>
+//                               </div>
+//                           </div>
+//                           <div class="selSizeArea">
+//                               <div class="danpinColorTitle">
+//                                   <p>
+//                                       尺码：</p>
+//                               </div>
+//                               <div class="selSize">
+//                                   <ul>
+//                                       <li id="onlickSelSize" title="10" name="15797968" class="">
+//                                           <p>26-28CM</p>
+//                                           <span></span>
+//                                       </li>
+//                                       <span class="colorHoverok"></span>
+//                                   </ul>
+//                               </div>
+//                               <div class="danpin_xuanzeGMcm" style="display:none;">
+//                                   <span style="height: 16px; display: block; width: 16px; background-position: -1911px 0pt; margin: 2px;float: left; " class="sprites"></span>
+//                                   <p>
+//                                       请选择您要购买的商品尺码</p>
+//                               </div>
+//                           </div>
+//                           <div class="blank8ie">
+//                           </div>
+//                           <div class="goodsNum">
+//                               <div class="danpinColorTitle" style="line-height: 18px;">
+//                                   <p>
+//                                       数量：</p>
+//                               </div>
+//                               <div class="danpinnumSelect">
+//                                   <select id="selectedAmount">
+//                                       <option value="1">
+//                                           1</option>
+//                                       <option value="2">
+//                                           2</option>
+//                                       <option value="3">
+//                                           3</option>
+//                                       <option value="4">
+//                                           4</option>
+//                                       <option value="5">
+//                                           5</option>
+//                                       <option value="6">
+//                                           6</option>
+//                                       <option value="7">
+//                                           7</option>
+//                                       <option value="8">
+//                                           8</option>
+//                                       <option value="9">
+//                                           9</option>
+//                                       <option value="10">
+//                                           10</option>
+//                                   </select>
+//                                   <span id="comeon" class="LastNum" style="display: inline;">余量有限</span>
+//                                   <span class="blank15"></span>
+//                               </div>
+//                           </div>
+//                           <span class="blank0"></span>
+//                           <div class="AreaItotal SelectGoods">
+//                               <div class="SelectAreaItotal SelectGoods">
+//                                   <div class="goodsAddArea SelectGoods">
+//                                       <div class="danpinColorTitle">
+//                                           <p class="SelectDetail">
+//                                               已选：</p>
+//                                       </div>
+//                                       <div class="goodsAdd">
+//                                           <p class="SelectName">藏青色+暖褐</p>
+//                                           <span class="SelectPoint">，</span>
+//                                           <p class="SelectSize">26-28CM</p>
+//                                           <p class="NowHasGoods">现在有货</p>
+//                                           <span class="blank0"></span>
+//                                       </div>
+//                                   </div>
+//                               </div>
+//                           </div>
+//                           <div class="shoppingNews">
+//                               <a id="nowbuy" name="item-item-select-shopping" href="#" class="btnnowbuy track">
+//                                   <span>立即购买</span>
+//                               </a>
+//                               <a id="addToShoppingCar" name="item-item-select-shopping" href="#" class="btnaddtocart track">
+//                                   <span>加入购物车</span>
+//                               </a>
+//                           </div>
+//                           <span class="blank20"></span>
+
+//                       </div>
+//                       <div class="blank0">
+//                       </div>
+//                   </div>
+//                   <div id="promotion">
+//                       <div class="danpin_YhTsBox danpin_YhTsTab ">
+//                           <ul>
+//                               <li>
+//                                   <span style="display: block; float: left;
+//           width: auto; height: 16px; background: #b81c22; margin: 4px 5px 0  0; color: #fff;
+//           line-height: 16px; text-align: center; padding: 0 5px;">
+//                                       免邮
+//                                   </span>
+//                                   全场购物满199元免运费
+//                                   <span class='cut'>优惠提示</span>
+//                               </li>
+//                           </ul>
+//                       </div>
+//                   </div>
+//                   <div class="blank15">
+//                   </div>
+//               </div>
+//           </div>
+//               `;
+//               console.log($('section'));
+//               $('section').html(str);
+//           }
+
+//       }
+//   }
+// }
+// var url='json/details.json';
+// sendAjax(url, options);
+
+
+// localStorage.removeItem('minelist');
 var big = (function () {
   return {
     init: function () {
@@ -148,5 +390,7 @@ $btn.onclick= function () {
   
   
 }//当前商品详情页只有这一个商品，若是其他商品页面，id会变；
+
+
 
 
---
+++ js/productlist.js
@@ -0,0 +1,67 @@
+
+var options = {
+    method: "get",
+    async: true,
+    data:null,
+    success: function(data){
+        
+        data=JSON.parse(data);
+        console.log(data);//数组对象；
+    //     [
+    //         {
+    //            "sid":"001",
+    //            "introduce":"ERYAN尔晏 抽象撞色纹中筒袜(男款)2双装",
+    //            "price":"78.00",
+    //             "src":"images/shop/load01.jpg"
+    //         },
+    //         {
+    //            "sid":"002",
+    //            "introduce":"ERY 克尔晏 画风撞色纹中筒袜(男款)1双装",
+    //            "price":"88.00",
+    //             "src":"images/shop/load02.jpg"
+    //         }
+    //    ]
+        var str=[];
+        for(var i=0;i<data.length;i++){
+            // console.log(data[i].sid);
+            str += `<li>
+            <div class="shop_pic" attr-aid='${data[i].sid}'>
+                    <a href='javascript:;'><img style='width:240px;' src="${data[i].src}"/></a>
+            </div>
+            <span class="sid" style="display:none">${data[i].sid}</span>
+            <p>${data[i].introduce}</p>
+            <div class="price">￥${data[i].price}</div>
+            
+        </li>`;
+                console.log(str);
+                $('.shop_box').html(str);
+
+        }
+    }
+}
+var url='json/shop.json';
+sendAjax(url, options);
+
+var $shop_box=document.querySelector('.shop_box');
+$shop_box.onclick=function(ev){
+    ev=ev||window.event;
+    var target=ev.target||ev.srcElement;
+    console.log(target.nodeName);
+    if(target.nodeName='IMG'){
+        var numn=target.parentNode.parentNode.getAttribute('attr-aid');
+        console.log(numn);
+        var _cookie=new OprationCookie();
+        _cookie.setCookie('hobbyid',numn);
+        console.log(document.cookie);
+        location.href = "productdetail.html";
+    }
+
+}
+// //点击商品图片跳转到详情页，同时将该商品信息保存至cookie，一次保存一个就行
+// $(".shop_box").delegate(".shop_pic","click",function(){
+//     var num = $(this).parent().find(".sid").html();
+//     //alert(num);
+//     setCookie("shopId",num);
+//     location.href = "details.html";
+// });
+
